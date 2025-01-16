92fbd02ccb7ae2702e72139401f930778a450edf
Néstor <nestor@nstlopez.com>
2025-01-16 18:06:11 +0100

    fix

--- host/@mf-types/remote/compiled-types/src/export-App.d.ts
+++ host/@mf-types/remote/compiled-types/src/export-App.d.ts
@@ -1,1 +1,13 @@
-export default function Component(): import("react").JSX.Element;
+import '@modern-js/runtime/registry/main';
+export declare const provider: () => {
+    render(info: import("@module-federation/bridge-react").RenderFnParams & {
+        [key: string]: unknown;
+    }): Promise<void>;
+    destroy(info: {
+        moduleName: string;
+        dom: HTMLElement;
+    }): Promise<void>;
+    rawComponent: import("react").ComponentType<any>;
+    __BRIDGE_FN__: (_args: any) => void;
+};
+export default provider;
